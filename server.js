742ce262286b6b124ad6d8a814c1e1d4d8fbba09
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-01-27 10:49:51 +0530

    migration and genrate

---
+++ server.js
@@ -0,0 +1,19 @@
+const {PrismaClient} = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+    try{
+        const newUser = await prisma.user.create({
+            data: {
+                name: "Anukool",
+                email: "ascmailing0@gmail.com",
+            }
+        })
+    }catch (err) {
+        console.log(err);
+    }finally{
+        prisma.$disconnect()
+    }
+}
+
