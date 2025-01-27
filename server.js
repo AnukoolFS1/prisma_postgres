6001418e6cd81ed9fc885f95a7dd1f4fa5b90378
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-01-27 13:28:32 +0530

    update with unique check

--- server.js
+++ server.js
@@ -5,26 +5,30 @@ const prisma = new PrismaClient();
 async function main() {
     try {
         // Create
-        // const newUser = await prisma.user.create({
-        //     data: {
+        // const newUser = await prisma.user.createMany({
+        //     data: [{
         //         name: "Anukool",
         //         email: "ascmailing0@gmail.com",
-        //     }
+        //     }, {
+        //         name: "Anukool Singh Chauhan",
+        //         email:"anukool26@outlook.com"
+        //     }]
         // })
+        // console.log(newUser)
 
-        // Read 
-        // const users = await prisma.user.findMany()
-        // console.log(users)
+        Read 
+        const users = await prisma.user.findMany()
+        console.log(users)
 
         // Update
-        // const data = await prisma.user.update({
-        //     where:{
-        //         id: 1  //always remember that there should be atleast one constraint in {where} which is either @unique or @id
-        //     }, data: {
-        //         name: "Anukool Chauhan",
-        //     }
-        // })
-        // console.log(data)
+        const data = await prisma.user.update({
+            where:{
+                id: 3  //always remember that there should be atleast one constraint in {where} which is either @unique or @id
+            }, data: {
+                email: "anukool26@gmail.com",
+            }
+        })
+        console.log(data)
 
         // Delete
         // const deletedUser = await prisma.user.delete({
