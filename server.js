46111e3f53373c5b1dbb86f42c9a89e73d82219e
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-01-27 13:21:04 +0530

    delete has been completed

--- server.js
+++ server.js
@@ -17,14 +17,23 @@ async function main() {
         // console.log(users)
 
         // Update
-        const data = await prisma.user.update({
-            where:{
-                id: 1
-            }, data: {
-                name: "Anukool Singh Chauhan",
-            }
-        })
-        console.log(data)
+        // const data = await prisma.user.update({
+        //     where:{
+        //         id: 1  //always remember that there should be atleast one constraint in {where} which is either @unique or @id
+        //     }, data: {
+        //         name: "Anukool Chauhan",
+        //     }
+        // })
+        // console.log(data)
+
+        // Delete
+        // const deletedUser = await prisma.user.delete({
+        //     where: {
+        //         email: "ascmailing0@gmail.com",
+        //         name: "Anukool Chauhan"
+        //     }
+        // })
+        // console.log(deletedUser)
     } catch (err) {
         console.log(err);
     } finally {
